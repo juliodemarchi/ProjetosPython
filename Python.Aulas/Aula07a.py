f0de9ac30cea8346f6496dd559f0bccf225c6d49
Julio Demarchi <129809896+juliodemarchi@users.noreply.github.com>
2026-02-02 17:32:43 -0400

    Add files via upload

---
+++ Python.Aulas/Aula07a.py
@@ -0,0 +1,16 @@
+# Operadores Aritméticos
+
+n1 = 5 + 2 == 7
+n2 = 5 - 2 == 3
+n3 = 5 * 2 == 10
+n4 = 5 / 2 == 2.5
+n5 = 5 ** 2 == 25
+n6 = 5 // 2 == 2
+n7 = 5 % 2 == 1
+
+# Ordem de Precendência (Prioridade)
+
+1 = ()
+2 = **
+3 = * / // %
+4 = +  -
